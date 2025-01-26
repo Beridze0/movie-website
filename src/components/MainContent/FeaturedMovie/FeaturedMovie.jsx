8eac3dac83e065ce7d51a753c0b9628b238a84dd
Beridze0 <beridzedavit904@gmail.com>
2025-01-26 18:26:56 +0400

    fixed sidebar media

---
+++ src/components/MainContent/FeaturedMovie/FeaturedMovie.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FeaturedMovie.css'
+
+const FeaturedMovie = () => {
+  return (
+    <div className='featured-movie-container'>
+
+    </div>
+  )
+}
+
+export default FeaturedMovie
---
+++ src/components/MainContent/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MainContent.css'
+
+const MainContent = () => {
+  return (
+    <div className='main-content-container'>
+
+    </div>
+  )
+}
+
+export default MainContent
